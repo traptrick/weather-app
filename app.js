b55902c70e52429a1c74dcfce912dda4d40d1429
traptrick <themk85@gmail.com>
2020-11-09 20:31:54 +0530

    added async await to fetch call

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
-var input = document.getElementById('ask');
-var btn = document.getElementById('btn');
+const input = document.getElementById('ask');
+const btn = document.getElementById('btn');
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const API = '5323cf0b57f729cb7c8ef2d8a4ec9ec8';
-btn.addEventListener('click', () => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${API}`)
-       .then(response => response.json())
-       .then(data => {
-          let kelvin = data.main.temp;
-          let celsius = Math.floor(kelvin - 273.15);
-          let fahrenheit = Math.floor(celsius * 1.8 + 32);
 
-          let iconId = data.weather[0].icon;
-          let description = data.weather[0].description;
-          let city = data.name;
-          let country = data.sys.country;
 
-          iconElement.innerHTML = `<img src="icons/${iconId}.png"/>`;
-          tempElement.innerHTML = `${celsius}°<span>C</span>`;
-          descElement.innerHTML =  `${description}`;
-          locationElement.innerHTML = `${city}, ${country}`;
+            const call = async () => {
+                 
+                  try {
+                        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${API}`);
+                        let data = await response.json();
 
-          tempElement.addEventListener('click', () =>{ 
-                  if( tempElement.innerHTML.includes("C") ){
-                        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
-                  }
-                  else{
+                        let kelvin = data.main.temp;
+                        let celsius = Math.floor(kelvin - 273.15);
+                        let fahrenheit = Math.floor(celsius * 1.8 + 32);
+                  
+                        let iconId = data.weather[0].icon;
+                        let description = data.weather[0].description;
+                        let city = data.name;
+                        let country = data.sys.country;
+                  
+                        iconElement.innerHTML = `<img src="icons/${iconId}.png"/>`;
                         tempElement.innerHTML = `${celsius}°<span>C</span>`;
+                        descElement.innerHTML =  `${description}`;
+                        locationElement.innerHTML = `${city}, ${country}`;
+                  
+                        tempElement.addEventListener('click', () =>{ 
+                                    if( tempElement.innerHTML.includes("C") ){
+                                          tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+                                    }
+                                    else{
+                                          tempElement.innerHTML = `${celsius}°<span>C</span>`;
+                                    }
+                        });   
+                  } catch (error) {
+                                iconElement.innerHTML = ``;
+                                 tempElement.innerHTML = ``;
+                                 descElement.innerHTML =  `🥺`;
+                                locationElement.innerHTML = `Oops! Place Not Found 🤷`;
                   }
-          });       
- }).catch( error => {
-         console.log(error);
-         iconElement.innerHTML = ``;
-          tempElement.innerHTML = ``;
-          descElement.innerHTML =  `🥺`;
-         locationElement.innerHTML = `Oops! Place Not Found 🤷`;
-        }        
- );
-});
+                             
+            }
+
+btn.addEventListener('click', call);
